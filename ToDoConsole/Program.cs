7cabe57a1c6d426ebf7aa88cdf50c3ef29493a56
sosuisen <hidekazu.kubota@gmail.com>
2024-02-17 15:31:56 +0900

    fix: add hints

--- ToDoConsole/Program.cs
+++ ToDoConsole/Program.cs
@@ -1,48 +1,39 @@
-namespace ToDoListConsole
+var toDoList = new List<string>();
+string userInput;
+do
 {
-    class Program
+    Console.WriteLine("\nToDo List:");
+    foreach (var item in toDoList)
     {
-        static void Main()
-        {
-            var toDoList = new List<string>();
-            string userInput;
-            do
-            {
-                Console.WriteLine("\nToDo List:");
-                foreach (var item in toDoList)
-                {
-                    Console.WriteLine("- " + item);
-                }
+        Console.WriteLine("- " + item);
+    }
 
-                Console.WriteLine(@"
+    Console.WriteLine(@"
 Choose an option:
 1. Add Task
 2. Remove Task
 3. Exit");
-                userInput = Console.ReadLine() ?? "";
+    userInput = Console.ReadLine() ?? "";
 
-                switch (userInput)
-                {
-                    case "1":
-                        Console.Write("Enter a task to add: ");
-
-                        // Add task to list
-                        // Use Console.ReadLine() to get a task
-
-
-
-                        break;
-                    case "2":
-                        Console.Write("Enter task index to remove: ");
-
-                        // Remove task from list
-                        // Use Console.ReadLine() to get an index number
+    switch (userInput)
+    {
+        case "1":
+            Console.Write("Enter a task to add: ");
+            var newTask = Console.ReadLine() ?? "";
+            // Add task to list
 
 
 
-                        break;
-                }
-            } while (userInput != "3");
-        }
+
+            break;
+        case "2":
+            Console.Write("Enter task index to remove: ");
+            var index = Console.ReadLine() ?? "";
+            // Remove task from list
+
+
+
+
+            break;
     }
-}
+} while (userInput != "3");
